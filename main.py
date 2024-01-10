3ff300762d95430feb1a12d2a970c0eb5b9cdcbe
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-01-10 15:37:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-# Built-in imports
+# Part 1
+def read_csv(filename):
+    # Type your code below
+    pass
 
+
+# Part 2
+def filter_gender(enrolment_by_age, sex):
+    # Type your code below
+    pass
+
+
+# Part 3
+def sum_by_year(enrolment):
+    # Type your code below
+    pass
+
+
+# Part 4
+def write_csv(filename, header, data):
+    # Type your code below
+    pass
+
+
+# TESTING
+# You can write code below to call the above functions
+# and test the output
